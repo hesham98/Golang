0767c3b41dcdfeada02b471fa75b1b3abbc8ca5b
hesham98 <h.elamalye2011@gmail.com>
2023-04-14 15:00:37 +0200

    for loop ractice

---
+++ forloop.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 0; i < 4; i++ {
+	// 	if i == 1 {
+	// 		break
+	// 	} else {
+	// 		fmt.Println("hi", i)
+	// 	}
+	// }
+
+	// for i := 0; i < 4; i++ {
+	// 	for j := 0; j < 3; j++ {
+	// 		if i == 1 && j == 1 {
+	// 			break
+	// 		} else {
+	// 			fmt.Println("x =", i, "y =", j)
+	// 		}
+	// 	}
+	// }
+
+	b := false
+	for i := 0; i < 4; i++ {
+		for j := 0; j < 3; j++ {
+			if i == 1 && j == 0 {
+				b = true
+				break
+			}
+			fmt.Println("x =", i, "y =", j)
+		}
+		if b == true {
+			break
+		}
+	}
+}
