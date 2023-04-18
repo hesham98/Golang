e0321fbd4f0931413b83c556f6fca0e33a07fab7
hesham98 <h.elamalye2011@gmail.com>
2023-04-18 05:42:44 +0200

    maps practice

---
+++ map.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// va := map[string]string{
+	// 	"age":  "24",
+	// 	"name": "hesham",
+	// }
+	// fmt.Println(va)
+	// fmt.Println(va["name"])
+
+	// players := make(map[string]bool)
+	// players["name"] = true
+	// fmt.Println(players)
+
+	// //this is function to delete from maps
+	// delete(va, "age")
+	// fmt.Println(va)
+
+	// 	v, ok := players["age"]
+	// 	fmt.Println("missing key", v, "ok", ok)
+
+	// 	v, ok = players["name"]
+	// 	fmt.Println("missing key", v, "ok", ok)
+
+	// he := []string{}
+	mapOfSlcie := map[string][]string{
+		"name": []string{"hesham", "mohamed", "Ali"},
+		"age":  []string{"24"},
+	}
+	fmt.Printf("map", mapOfSlcie["name"])
+
+}
