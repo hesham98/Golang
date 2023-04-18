fdbaff38165006a46bc98e0d492dd9b08669d611
hesham98 <h.elamalye2011@gmail.com>
2023-04-18 18:03:44 +0200

    function practice

---
+++ function.go
@@ -0,0 +1,46 @@
+package main
+
+// func addPrint(a int, b int) {
+// 	fmt.Println(a + b)
+// }
+
+func add(a int, b int) int {
+	return a + b
+}
+
+// func sub(a, b int) {
+// 	fmt.Println(a - b)
+// }
+
+// func l(para ...interface{}) {
+// 	fmt.Println("number of parameters", len(para))
+// }
+
+// func conc(a, b string) (s string, i int) {
+// 	s = a + b
+// 	i = len(s)
+// 	return s, i
+// }
+
+// func common(bbb bool) (string, error) {
+// 	if bbb {
+// 		return "OK", nil
+// 	} else {
+// 		return "", errors.New("this is an error")
+// 	}
+// }
+
+func main() {
+	// addPrint(2, 7)
+	// fmt.Println(add(9, 11))
+	// v := add(7, 88)
+	// fmt.Println(v)
+	// l("hesham", 24, true)
+	// sub(99, 42)
+	// fmt.Println(conc("hes", "moh"))
+	// fmt.Println(common(false))
+
+	// var x func(int, int) int
+	// x = add
+	// fmt.Println(x(4, 77))
+}
