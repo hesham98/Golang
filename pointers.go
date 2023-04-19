4903cad81eebcadc3ca8c1c7fdfd71cef9e81f73
hesham98 <h.elamalye2011@gmail.com>
2023-04-19 17:31:50 +0200

    pointer practice

--- pointers.go
+++ pointers.go
@@ -1,24 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	// age := 25
-	// pAge := &age
-	// fmt.Println(*pAge)
+	age := 25
+	pAge := &age
+	fmt.Println(*pAge)
 
-	// name := "Main"
-	// fmt.Println("the main value", name)
-	// updateCopy(name)
-	// fmt.Println("after update Copy", name)
-	// updateRef(&name)
-	// fmt.Println("after update Refernce", name)
+	name := "Main"
+	fmt.Println("the main value", name)
+	updateCopy(name)
+	fmt.Println("after update Copy", name)
+	updateRef(&name)
+	fmt.Println("after update Refernce", name)
 }
 
-// func updateCopy(s string) {
-// 	s = "Copy"
-// }
+func updateCopy(s string) {
+	s = "Copy"
+}
 
-// func updateRef(s *string) {
-// 	fmt.Println("address is ", s)
-// 	*s = "Ref"
-// }
+func updateRef(s *string) {
+	fmt.Println("address is ", s)
+	*s = "Ref"
+}
