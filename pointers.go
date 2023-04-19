cd8989181c092eb2b14a95b601aba7da3de47541
hesham98 <h.elamalye2011@gmail.com>
2023-04-19 17:31:07 +0200

    pointer practice

---
+++ pointers.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	// age := 25
+	// pAge := &age
+	// fmt.Println(*pAge)
+
+	// name := "Main"
+	// fmt.Println("the main value", name)
+	// updateCopy(name)
+	// fmt.Println("after update Copy", name)
+	// updateRef(&name)
+	// fmt.Println("after update Refernce", name)
+}
+
+// func updateCopy(s string) {
+// 	s = "Copy"
+// }
+
+// func updateRef(s *string) {
+// 	fmt.Println("address is ", s)
+// 	*s = "Ref"
+// }
